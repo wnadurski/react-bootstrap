295dc76e7a3f2c03586f990faf0b3f36fe38da48
wnadurski <w.nadurski@gmail.com>
2017-03-20 22:11:17 +0100

    Project bootstrap

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from 'react-dom'
+
+import './styles/main.scss'
+
+const World = () => (
+    <h1>Hello world</h1>
+)
+
+ReactDOM.render(<World/>, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path')
+var webpack = require('webpack')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+    entry: './main.jsx',
+    output: {
+        path: path.join(__dirname, "dist"),
+        publicPath: "/assets/",
+        filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react', 'stage-2']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('css-loader!sass-loader')
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'styles.css',
+            allChunks: true
+        })
+    ]
+}
